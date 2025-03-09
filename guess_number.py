3fed61362bd48cf6d2b7e9bb06165d4d9f0b0df3
Vladimir Novikov <novikov-vl@yandex.ru>
2025-03-09 23:38:12 +0300

    add initial commit

---
+++ guess_number.py
@@ -0,0 +1,39 @@
+# Импорт функции получения случайных чисел из модуля random.
+from random import randint
+
+# Получаем случайное число в диапазоне от 1 до 100.
+number = randint(1, 100)
+print('Угадайте число от 1 до 100')
+
+def attempt(number):
+    if 10 < number < 20:
+        return 'попыток!'
+    elif number % 10 == 1:
+        return 'попытку!'
+    elif 2 <= number % 10 <= 4:
+        return 'попытки!'
+    else:
+        return 'попыток!'
+
+def main():
+    counter = 0
+    while True:
+        # Получаем число от пользователя и сохраняем его в переменную.
+        guess = int(input('Введите число: '))
+        counter += 1
+        # Если число меньше загаданного...
+        if guess < number:
+            # ...выводим сообщение.
+            print('Надо больше')
+        # Если число больше загаданного...
+        elif guess > number:
+            print('Надо меньше')
+        # Если число угадано...
+        else:
+            # ...прерываем выполнение программы и...
+            return counter
+
+    # выводим сообщение.
+attempts = main()
+print('Отличная интуиция! Вы угадали число за',
+      attempts, attempt(attempts))
